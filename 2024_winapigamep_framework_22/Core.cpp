996f47d772fbed8be182d9f6a0ea28749287b094
BashMing10207 <backupdate102@gmail.com>
2024-12-02 14:55:09 +0900

    GamePlayerFix

--- 2024_winapigamep_framework_22/Core.cpp
+++ 2024_winapigamep_framework_22/Core.cpp
@@ -100,6 +100,7 @@ void Core::MainRender()
 {
 	::PatBlt(m_hBackDC, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, BLACKNESS);
 
+
 	HBRUSH brush = CreateSolidBrush(RGB(128,128,128));
 	HBRUSH oldbrush = (HBRUSH)SelectObject(m_hBackDC, brush);
 
