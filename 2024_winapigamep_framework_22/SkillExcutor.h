d63c3ca97bc814971bd8d8873afd5c76c2b010ad
BashMing10207 <backupdate102@gmail.com>
2024-12-06 22:19:43 +0900

    a

--- 2024_winapigamep_framework_22/SkillExcutor.h
+++ 2024_winapigamep_framework_22/SkillExcutor.h
@@ -4,18 +4,6 @@
 class SkillExcutor :
 	public Component
 {
-	void GetAction(Item item, Vec2 direction);
-	void Move(Vec2 direction);
-	void Gun(Vec2 direction);
-	void Grenade(Vec2 direction);
-	void OilGrenade(Vec2 direction);
-	void ThrowMagnet(Vec2 direction);
-	void ThrowSeed(Vec2 direction);
-	void Magnet(Vec2 direction);
-	void ExplosionSkill(Vec2 direction);
-	void JooYoung(Vec2 direction);
-	void BeWater(Vec2 direction);
-	void Tetkkai(Vec2 direction);
 	void Teleport(Vec2 direction);
 public:
     void LateUpdate() override;
