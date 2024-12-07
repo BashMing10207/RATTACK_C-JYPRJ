20e009c481b493db8e9aaa412c0240794dd5ae9c
gksdnfgksdnf <kkho8489@gmail.com>
2024-12-07 16:18:25 +0900

    itemSelected

--- 2024_winapigamep_framework_22/UI.cpp
+++ 2024_winapigamep_framework_22/UI.cpp
@@ -1,6 +1,7 @@
 ﻿#include "pch.h"
 #include "InputManager.h"
 #include "UI.h"
+#include "TimeManager.h"
 
 UI::UI()
 	: m_vPos{}
@@ -8,6 +9,10 @@ UI::UI()
 	, m_isActive{ false }
 	, m_parent{ nullptr }
 	, m_text{ L"" }
+	, m_isMoveing{ false }
+	, m_currentTime{ 0.f }
+	, m_moveTime{ 0.f }
+	, m_isMoved{ true }
 {
 
 }
@@ -24,14 +29,13 @@ void UI::Init()
 
 void UI::Update()
 {
-
 }
 
 void UI::Render(HDC _hdc)
 {
 	for (auto& child : m_children)
 	{
-		if (child && child->GetActive()) 
+		if (child && child->GetActive())
 		{
 			child->Render(_hdc);
 		}
@@ -43,3 +47,16 @@ void UI::RenderText(HDC _hdc)
 
 }
 
+void UI::SetPosLerp(Vec2 _targetPos, float _moveTime)
+{
+	m_isMoveing = true;
+	m_targetPos = _targetPos;  // 목표 위치 설정
+	m_moveTime = _moveTime;    // 이동 시간 설정
+	m_currentTime = 0.0f;     // 경과 시간 초기화
+	m_isMoved = false;
+}
+
+float UI::Lerp(float a, float b, float t)
+{
+	return a + (b - a) * t;
+}
